09cc2a37fc8ce54e270c3160ac4cf41663648d54
idan <58389697+idanbn@users.noreply.github.com>
2022-04-30 19:25:10 +0300

    text to speech

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import React from 'react';
 
 export default function App() {
+  const [name, setName] = React.useState("");
+
+  const listAllVoiceOptions = async () => {
+    let voices = await Speech.getAvailableVoicesAsync();
+    console.log(voices);
+  };
+
+  React.useEffect(listAllVoiceOptions);
+
+  const speakGreeting = () => {
+      const greeting = `Hi ${name}`;
+      const options = {
+        voice: "com.apple.speech.synthesis.voice.Fred",
+        pitch: 1.5,
+        rate: 0.7
+      };
+      Speech.speak(greeting, options)
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={setName} value={name} />
+      <Button title="Speak" onPress={speakGreeting} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    alignSelf: 'stretch',
+    height: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "red",
+    margin: 8
+  }
 });
